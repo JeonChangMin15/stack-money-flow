b6396b411e111030c896cddfa54403399b4bbbe8
ChangMin <cckdals222@gmail.com>
2025-04-26 14:57:29 +0900

    [기타] 토큰 수정

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,7 +3,7 @@ module.exports = {
   siteUrl: 'https://stack-money-flow.vercel.app/',
   generateRobotsTxt: true,
   changefreq: 'daily',
-  priority: 0.7,
+  priority: 1.0,
   sitemapSize: 5000,
   outDir: './public',
 };
